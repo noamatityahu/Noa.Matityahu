407f7ec00f39ea534786de4a8d433d6bd3b91d26
noamatityahu <73295862+noamatityahu@users.noreply.github.com>
2021-12-01 16:42:27 +0200

    Homework 6

---
+++ CV.js
@@ -0,0 +1,8 @@
+function valid() {
+    const inpObj = document.getElementById("phone");
+    if (!inpObj.checkValidity()) {
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    } else {
+        document.getElementById("demo").innerHTML = "Input OK";
+    }
+}
